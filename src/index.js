183cfc062e52a7bcfb94c7701d56b0404e5647a5
Li Kai <falconets25601@gmail.com>
2018-05-11 15:50:54 +0800

    add firebase dependency

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// Firebase App is always required and must be first
+import * as firebase from 'firebase/app';
+
+// Add additional services you want to use
+import "firebase/auth";
+// import "firebase/database";
+// import "firebase/firestore";
+// import "firebase/messaging";
+// import "firebase/functions";
+// import "firebase/storage";
+
+const config = {
+  apiKey: "AIzaSyByfXJU9SK5OQAbZfxCw1oUG4M1R1hS4hE",
+  authDomain: "hack-n-roll.firebaseapp.com",
+  databaseURL: "https://hack-n-roll.firebaseio.com",
+  projectId: "hack-n-roll",
+  storageBucket: "",
+  messagingSenderId: "1000953651325"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));

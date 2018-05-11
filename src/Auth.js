03dd61b5c69e121a75ad322ae69fb12536f90a5d
E-Liang Tan <eliang@eliangtan.com>
2018-05-11 18:35:05 +0800

    Add test form

--- src/Auth.js
+++ src/Auth.js
@@ -1,5 +1,18 @@
 import React, { PureComponent } from 'react';
 import './Auth.css';
+import Form from 'react-jsonschema-form';
+
+const schema = {
+  title: 'Formo',
+  type: 'object',
+  required: ['title'],
+  properties: {
+    title: { type: 'string', title: 'Title', default: 'A new task' },
+    done: { type: 'boolean', title: 'Done?', default: false },
+  },
+};
+
+const log = (type) => console.log.bind(console, type);
 
 /**
  * Auth renders the form for communication with AuthContainer
@@ -7,7 +20,12 @@ import './Auth.css';
 class Auth extends PureComponent {
   render() {
     return (
-      <form></form>
+      <Form
+        schema={schema}
+        onChange={log('changed')}
+        onSubmit={log('submitted')}
+        onError={log('errors')}
+      />
     );
   }
 }

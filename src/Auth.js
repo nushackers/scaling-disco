275efe6ee6760b832013edfd49a6ad43a7fa3dcd
Li Kai <falconets25601@gmail.com>
2018-05-11 16:46:37 +0800

    add auth components

---
+++ src/Auth.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+import './Auth.css';
+
+/**
+ * Auth renders the form for communication with AuthContainer
+ */
+class Auth extends PureComponent {
+  render() {
+    return (
+      <form></form>
+    );
+  }
+}
+
+export default Auth;
---
+++ src/AuthContainer.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+import Auth from './Auth';
+
+/**
+ * AuthContainer is in charge of handling auth logic.
+ */
+class AuthContainer extends PureComponent {
+  render() {
+    return (
+      <Auth />
+    );
+  }
+}
+
+export default AuthContainer;

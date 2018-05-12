7e2e5995d15d3f4a4718bf6bcdeef77e40855134
Li Kai <falconets25601@gmail.com>
2018-05-12 17:00:31 +0800

    add links for groups

--- src/groups/Group.js
+++ src/groups/Group.js
@@ -1,4 +1,5 @@
 import React, { PureComponent, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 /**
  * Groups stuff lol
@@ -9,9 +10,15 @@ class Group extends PureComponent {
       <Fragment>
         <div>Hi, you are not in any group.</div>
         <ul>
-          <li><a href='groups/new'>Create new group</a></li>
-          <li><a href='groups/join'>Join group</a></li>
-          <li><a href='groups/see'>See groups</a></li>
+          <li>
+            <Link to="/groups/new">Create new group</Link>
+          </li>
+          <li>
+            <Link to="/groups/join">Join group</Link>
+          </li>
+          <li>
+            <Link to="/groups/see">See groups</Link>
+          </li>
         </ul>
       </Fragment>
     );

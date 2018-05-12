fb756170591caa93721ad44d23ca5d2f1883389f
Li Kai <falconets25601@gmail.com>
2018-05-12 18:05:17 +0800

    add mobile navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
-import { Route, Redirect, NavLink } from 'react-router-dom';
-import firebase, { db } from './Firebase';
+import { Route, NavLink } from 'react-router-dom';
+import firebase from './Firebase';
+import Nav from './Nav';
 import Home from './Home';
 import Signup from './auth/SignupContainer';
 import Login from './auth/LoginContainer';
@@ -43,45 +44,42 @@ class App extends Component {
     const { user } = this.state;
     return (
       <Fragment>
-        <nav className="navbar navbar-light bg-light">
-          <NavLink to="/">Home</NavLink>
-          <ul className="nav">
-            <NavLink className="nav-link" to="/projects">
-              Projects
-            </NavLink>
-            {user ? (
-              <Fragment>
-                <li className="nav-item">Welcome, {user.displayName}</li>
-                <NavLink className="nav-link" to="/groups">
-                  Groups
+        <Nav>
+          <NavLink className="nav-link" to="/projects">
+            Projects
+          </NavLink>
+          {user ? (
+            <Fragment>
+              <li className="nav-item">Welcome, {user.displayName}</li>
+              <NavLink className="nav-link" to="/groups">
+                Groups
+              </NavLink>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/projects/new">
+                  Submit Project
                 </NavLink>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/projects/new">
-                    Submit Project
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <button className="btn btn-link nav-link" onClick={this.onSignOut}>
-                    Log out
-                  </button>
-                </li>
-              </Fragment>
-            ) : (
-              <Fragment>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/login">
-                    Log In
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/signup">
-                    Sign Up
-                  </NavLink>
-                </li>
-              </Fragment>
-            )}
-          </ul>
-        </nav>
+              </li>
+              <li className="nav-item">
+                <button className="btn btn-link nav-link" onClick={this.onSignOut}>
+                  Log out
+                </button>
+              </li>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Log In
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/signup">
+                  Sign Up
+                </NavLink>
+              </li>
+            </Fragment>
+          )}
+        </Nav>
         <main className="container">
           <ErrorAlert error={this.state.error} onDismiss={this.onDismissAlert} />
           <Route path="/" component={Home} exact />
---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { PureComponent } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Nav extends PureComponent {
+  state = {
+    isToggleOn: false,
+  };
+
+  showDropdown = () =>
+    this.setState((prevState) => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
+
+  render() {
+    const { isToggleOn } = this.state;
+    const classDropdownMenu = `navbar-collapse collapse ${this.state.isToggleOn ? ' show' : ''}`;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <NavLink to="/">Home</NavLink>
+        <button
+          className="navbar-toggler"
+          id="navbarDropdown"
+          aria-expanded={isToggleOn}
+          onClick={this.showDropdown}
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+        <div className={classDropdownMenu}>
+          <ul className="nav navbar-nav">{this.props.children}</ul>
+        </div>
+      </nav>
+    );
+  }
+}
+export default Nav;
